77060906ae7ed25839b45a63dd20860cfca4b740
Bryan Pyo <bryan@axiomatic-ai.com>
2025-08-18 23:42:49 -0400

    adding param parameter to fix faulty api request

--- axiomatic_mcp/servers/plot_parser/server.py
+++ axiomatic_mcp/servers/plot_parser/server.py
@@ -1,24 +1,24 @@
 """Documents MCP server for filesystem document operations."""
 
+import random
 from pathlib import Path
 from typing import Annotated
-import random
 
 from fastmcp import FastMCP
 
 from ...shared import AxiomaticAPIClient
 
+
 def process_plot_parser_output(response_json, max_points: int = 100, sig_figs: int = 5) -> str:
     condensed_response = []
-    for extracted_series in response_json['extracted_series']:
-        all_extracted_points = extracted_series['points']
-        selected_points = random.sample(all_extracted_points, max_points)
+
+    for extracted_series in response_json["extracted_series"]:
+        all_extracted_points = extracted_series["points"]
+        selected_points = random.sample(all_extracted_points, min(max_points, len(all_extracted_points)))
         condensed_points_list = []
         for point in selected_points:
-            condensed_points_list.append((format(point['value_x'], f'.{sig_figs}g'), format(point['value_y'], f'.{sig_figs}g')))
-        condensed_response.append({'id': extracted_series['id'],
-                                   'color': extracted_series['color'],
-                                   'points': condensed_points_list})
+            condensed_points_list.append((format(point["value_x"], f".{sig_figs}g"), format(point["value_y"], f".{sig_figs}g")))
+        condensed_response.append({"id": extracted_series["id"], "color": extracted_series["color"], "points": condensed_points_list})
     return str(condensed_response)
 
 
@@ -32,7 +32,7 @@ plot_parser_server = FastMCP(
 @plot_parser_server.tool(
     name="extract_data_from_plot_image",
     description="Extracts data from an image of a plot",
-    tags=["plot", "filesystem", "analyze"],
+    tags={"plot", "filesystem", "analyze"},
 )
 async def extract_data_from_plot_image(
     plot_path: Annotated[Path, "The absolute path to the image file of the plot to analyze"],
@@ -40,10 +40,9 @@ async def extract_data_from_plot_image(
     if not plot_path.exists():
         raise FileNotFoundError(f"Document not found: {plot_path}")
 
-    data = {"get_img_coords": "true", "v2": "true"}
-
     with Path.open(plot_path, "rb") as f:
         files = {"plot_img": ("plot.png", f, "image/png")}
-        response = AxiomaticAPIClient().post("/document/plot/points", files=files, data=data)
+        params = {"get_img_coords": True, "v2": True}
+        response = AxiomaticAPIClient().post("/document/plot/points", files=files, params=params)
 
     return process_plot_parser_output(response)
