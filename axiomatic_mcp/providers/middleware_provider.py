0e4a34be666eb52265133f8a992307caca6e5741
a96lex <a96lex@gmail.com>
2025-09-15 10:24:49 +0200

    chore: add lru cache

--- axiomatic_mcp/providers/middleware_provider.py
+++ axiomatic_mcp/providers/middleware_provider.py
@@ -1,9 +1,12 @@
+from functools import lru_cache
+
 from fastmcp.server.middleware import Middleware
 
 from ..config.config import get_settings
 from ..shared.middleware.moesif_mcp_middleware import MoesifMcpMiddleware
 
 
+@lru_cache
 def get_mcp_middleware() -> list[Middleware]:
     # This is our Publishable Application Id
     app_settings = get_settings().app
