c214e4ab5a6c24a5875ef7563c0655551e92b5fb
AdriantrejoAx <adrian.trejo@axiomatic-ai.com>
2025-11-10 16:41:42 +0100

    fix:remove moesif as it is not needed

--- axiomatic_mcp/config/config.py
+++ axiomatic_mcp/config/config.py
@@ -1,6 +1,5 @@
 from functools import lru_cache
 
-from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -8,16 +7,8 @@ class _BaseConfig(BaseSettings):
     model_config = SettingsConfigDict()
 
 
-class _AppSettings(_BaseConfig):
-    moesif_application_id: str | None = (
-        "eyJhcHAiOiI0OTM6MjY5MSIsInZlciI6IjIuMSIsIm9yZyI6IjQyMDo2MzIiLCJwdWIiOnRydWUsImlhdCI6MTc1NjY4NDgwMH0."
-        "DcpUTfu3KHdtySxt6VksTMVD5TQyd2AAsO9UGYqvF9s"
-    )
-    disable_telemetry: bool = Field(default=False, alias="DISABLE_TELEMETRY")
-
-
 class _Settings(_BaseConfig):
-    app: _AppSettings = _AppSettings()
+    app: _BaseConfig = _BaseConfig()
 
 
 @lru_cache
