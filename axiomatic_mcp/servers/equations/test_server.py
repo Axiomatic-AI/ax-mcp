a0a33d21624daeaa822da30fa8bc08fcaafe71b8
tymekaxai <tymek@axiomatic-ai.com>
2025-11-19 17:44:19 +0100

    sending pdf works, sending markdown doesnt...

--- axiomatic_mcp/servers/equations/test_server.py
+++ axiomatic_mcp/servers/equations/test_server.py
@@ -8,7 +8,11 @@ sample_markdown_path = Path("/Users/tymek_axai/Desktop/Quantum_basics.md")
 sample_task = "Check if the first equation is correct"
 
 with AxiomaticAPIClient() as client:
-    response = client.post(
-        "/equations/derive", data={"task": sample_task}, files={"pdf_file": (sample_pdf_path.name, sample_pdf_path.read_bytes(), "application/pdf")}
+    response_pdf = client.post(
+        "/equations/check", data={"task": sample_task}, files={"pdf_file": (sample_pdf_path.name, sample_pdf_path.read_bytes(), "application/pdf")}
     )
-    print(response)
+    print(response_pdf)
+    print("--------------------------------")
+    print("--------------------------------")
+    response_markdown = client.post("/equations/check", data={"task": sample_task, "markdown": sample_markdown_path.read_text(encoding="utf-8")})
+    print(response_markdown)
